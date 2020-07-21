c6e4d36f2a2440f8b378a91167c7c2fbf38dd48d
M. Mocellin <michele.mocellin93@alice.it>
2020-07-21 21:38:02 +0200

    First Commit

---
+++ main/SD_card.c
@@ -0,0 +1,5 @@
+//
+// Created by Michele Mocellin on 21/07/2020.
+//
+
+#include "SD_card.h"
---
+++ main/SD_card.h
@@ -0,0 +1,8 @@
+//
+// Created by Michele Mocellin on 21/07/2020.
+//
+
+#ifndef SD_CARD_SD_CARD_H
+#define SD_CARD_SD_CARD_H
+
+#endif //SD_CARD_SD_CARD_H
